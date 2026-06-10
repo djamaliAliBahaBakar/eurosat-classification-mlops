fe008b6ef93a82aa7f3c5cbe578e00011b71b18d
Djamali Ali bakar <dalibakar@gmail.com>
2026-06-10 23:34:00 +0200

    evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,3 +1,4 @@
 
 
-def evaluate():
+def evaluate(model,test_ds_prepared, class_names):
+    loss, accuracy, top2_accuracy = model.evaluate(test_ds_prepared, verbose=0)
--- src/train.py
+++ src/train.py
@@ -40,7 +40,7 @@ def compile_model(model, learning_rate):
     model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),  
     loss='sparse_categorical_crossentropy',
-    metrics=['accuracy', keras.metrics.TopKCategoricalAccuracy(k=2, name='top2_accuracy'), 'precision', 'recall'])
+    metrics=['accuracy', keras.metrics.TopKCategoricalAccuracy(k=2, name='top2_accuracy')])
 
     
 
